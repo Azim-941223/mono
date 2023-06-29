313a9e213ec820d423e2df45a2cebcbcd31171ce
laku <110287809+lakuuu@users.noreply.github.com>
2023-06-29 19:33:35 +0800

    feat: i18next

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import './App.scss'
+import { useTranslation } from 'react-i18next'
+import "./i18next"
 
 
 const App:React.FC = () => {
+  const { t, i18n } = useTranslation();
+  const changeLanguage = (language: string) => {
+    i18n.changeLanguage(language);
+  }
   return (
     <div className='App'>
-      <h1>App</h1>
+      <h1>{t("App.title")}</h1>
+      <button onClick={() => changeLanguage("en")}>EN</button>
+      <button onClick={() => changeLanguage("ru")}>RU</button>
     </div>
   )
 }
