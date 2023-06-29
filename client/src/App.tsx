20ca3fb213b49fdaf81a2abafbf550f4caefe82d
laku <110287809+lakuuu@users.noreply.github.com>
2023-06-30 00:12:49 +0800

    feat: fix bug

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,12 @@
 import React from 'react'
 import './App.scss'
-import { useTranslation } from 'react-i18next'
 import "./i18next"
 
 
 const App:React.FC = () => {
-  const { t, i18n } = useTranslation(["maigitn"]);
-  const changeLanguage = (language: string) => {
-    i18n.changeLanguage(language);
-  }
   return (
     <div className='App'>
-      <h1>{t("App.title")}</h1>
-      <button onClick={() => changeLanguage("en")}>EN</button>
-      <button onClick={() => changeLanguage("ru")}>RU</button>
+      <h1>app</h1>
     </div>
   );
 };
