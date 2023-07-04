5d94a57e163e91586acc44c92924116679f860e1
Akmaral <mdktva@gmail.com>
2023-07-04 23:27:03 +0600

    set up react-roter-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,23 @@
-import React from 'react'
-import './App.scss'
-import { useTranslation } from 'react-i18next'
-import "./i18next"
-
-
-const App:React.FC = () => {
-  const { t, i18n } = useTranslation(["maigitn"]);
+import React from "react";
+import "./App.scss";
+import { useTranslation } from "react-i18next";
+import { Route, Routes } from "react-router-dom";
+import "./i18next";
+import Test from "./Test";
+const App: React.FC = () => {
+  const { t, i18n } = useTranslation(["main"]);
   const changeLanguage = (language: string) => {
     i18n.changeLanguage(language);
-  }
+  };
   return (
-    <div className='App'>
+    <div className="App">
       <h1>{t("App.title")}</h1>
       <button onClick={() => changeLanguage("en")}>EN</button>
       <button onClick={() => changeLanguage("ru")}>RU</button>
+      <Routes>
+        <Route path="/test" element={<Test />} />
+      </Routes>
+    </div>
   );
 };
-
 export default App;
---
+++ client/src/Test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Test = () => {
+  return (
+    <div>Testing page</div>
+  );
+};
+
+export default Test;
