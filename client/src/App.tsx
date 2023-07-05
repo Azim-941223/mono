97cab210ff2a471a315852a5a441ab8c87db5edd
Akmaral <mdktva@gmail.com>
2023-07-05 21:28:10 +0600

    removed testing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import "./App.scss";
 import { useTranslation } from "react-i18next";
-import { Route, Routes } from "react-router-dom";
 import "./i18next";
-import Test from "./Test";
 const App: React.FC = () => {
   const { t, i18n } = useTranslation(["main"]);
   const changeLanguage = (language: string) => {
@@ -14,9 +12,6 @@ const App: React.FC = () => {
       <h1>{t("App.title")}</h1>
       <button onClick={() => changeLanguage("en")}>EN</button>
       <button onClick={() => changeLanguage("ru")}>RU</button>
-      <Routes>
-        <Route path="/test" element={<Test />} />
-      </Routes>
     </div>
   );
 };
--- client/src/Test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Test = () => {
-  return (
-    <div>Testing page</div>
-  );
-};
-
-export default Test;
