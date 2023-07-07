955125bb47f4cf5af2c25787202d44d7f0786ff0
Алмазбек Ишенбеков <ishenbekovalmaz@gmail.com>
2023-07-07 13:32:38 +0600

    feat: database added(MongoDB)

--- server/server.js
+++ server/server.js
@@ -1,8 +1,25 @@
-require("dotenv").config()
-const express = require("express");
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
 
-const app = express()
+mongoose
+  .connect(
+    "mongodb+srv://Almazbek:wwwwww@cluster0.62i5xjd.mongodb.net/mono?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("DB ok"))
+  .catch((err) => console.log("DB error", err));
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!")
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`liste port ${process.env.PORT}`);
-})
+});
