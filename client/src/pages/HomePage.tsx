300a7ae7e29ae092eab28c1dacdb201b833fbb89
Алмазбек Ишенбеков <ishenbekovalmaz@gmail.com>
2023-07-11 20:52:01 +0600

    feat: finished without adaptive

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import NewProductsSlider from "../components/NewProductsSlider/NewProductsSlider";
 import PopularCategories from "../components/PopularCategories/PopularCategories";
 
 function HomePage() {
   return (
     <main>
       <PopularCategories />
+      <NewProductsSlider />
     </main>
   );
 }
