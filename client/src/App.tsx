e1c9ceb9f83743513f7f980920e583d542908028
Алмазбек Ишенбеков <ishenbekovalmaz@gmail.com>
2023-07-12 16:11:17 +0600

    fix: tsconfig

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,7 @@
+import React from "react";
 
-import React from 'react'
+const App: React.FC = () => {
+  return <div className="App">App</div>;
+};
 
-const App:React.FC = () => {
-  return (
-    <div className='App'>App</div>
-  )
-}
-
-export default App
+export default App;
