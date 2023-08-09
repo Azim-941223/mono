86ee69342f420c11ac69ff75abcab872b3d220e7
laku <110287809+lakuuu@users.noreply.github.com>
2023-08-10 00:53:27 +0800

    Revert "Feat/catalog"

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,7 +8,6 @@ const App: React.FC = () => {
       <SliderComponent />
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import CatalogPage from "./pages/CatalogPage";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 
@@ -16,9 +15,9 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <Header />
+
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/catalog" element={<CatalogPage/>} />
       </Routes>
 
       <Footer />
