03cb2f7abadd0c1bb8a5478be8e6843fb6c3dd43
Akmaral <mdktva@gmail.com>
2023-08-10 20:11:52 +0600

    did furniture page in catalog

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,24 +2,21 @@ import React from "react";
 import "./App.scss";
 import "./i18next";
 import SliderComponent from "./components/slider/SliderComponent";
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <SliderComponent />
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import FurnitureCatalog from "./components/FurnitureCatalog/FurnitureCatalog";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Header />
-
+      <SliderComponent />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/furniture" element={<FurnitureCatalog />} />
       </Routes>
-
       <Footer />
     </div>
   );
