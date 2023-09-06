1e34c2284980569a16a6fceb9649d8dcf1687890
Akmaral <mdktva@gmail.com>
2023-09-06 22:48:42 +0600

    did chairs component in catalog

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,6 +9,7 @@ import Header from "./components/Header/Header";
 import FurnitureCatalog from "./components/FurnitureCatalog/FurnitureCatalog";
 import InteriorItems from "./components/InteriorItems/InteriorItems";
 import Beds from "./components/Catalog/Beds/Beds";
+import Chairs from "./components/Catalog/Chairs/Chairs";
 
 const App: React.FC = () => {
   return (
@@ -20,6 +21,7 @@ const App: React.FC = () => {
         <Route path="/furniture" element={<FurnitureCatalog />} />
         <Route path="/interior" element={<InteriorItems />} />
         <Route path="/beds" element={<Beds />} />
+        <Route path="/chairs" element={<Chairs />} />
       </Routes>
       <Footer />
     </div>
